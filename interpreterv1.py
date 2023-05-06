cd54ce3db764ad2adce04f32914e24ab247ef0e6
jz_2003 <zhaojianxing2003@gmail.com>
2023-05-05 23:01:38 -0700

    lil thing

--- interpreterv1.py
+++ interpreterv1.py
@@ -47,40 +47,40 @@ class Interpreter(InterpreterBase):
 
 
 
-def read_file(file_path):
-    """Reads a file of text and returns a list of strings."""
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-        lines = [line.strip() for line in lines]
-        return lines
+# def read_file(file_path):
+#     """Reads a file of text and returns a list of strings."""
+#     with open(file_path, 'r') as file:
+#         lines = file.readlines()
+#         lines = [line.strip() for line in lines]
+#         return lines
 
-def print_line_nums(parsed_program):
-    for item in parsed_program:
-        if type(item) is not list:
-            print(f'{item} was found on line {item.line_num}')
-        else:
-            print_line_nums(item)
+# def print_line_nums(parsed_program):
+#     for item in parsed_program:
+#         if type(item) is not list:
+#             print(f'{item} was found on line {item.line_num}')
+#         else:
+#             print_line_nums(item)
 
 
-def main():
-    # program_source = ['(class main',
-    # ' (method main ()',
-    # ' (print "hello world!")',
-    # ' ) # end of method',
-    # ') # end of class']
-    # program_source = ""
+# def main():
+#     # program_source = ['(class main',
+#     # ' (method main ()',
+#     # ' (print "hello world!")',
+#     # ' ) # end of method',
+#     # ') # end of class']
+#     # program_source = ""
 
-    file_path = "./codeExample3.brewin"
-    program_source = read_file(file_path=file_path)
-    # this is how you use our BParser class to parse a valid
-    # Brewin program into python list format.
-    result, parsed_program = BParser.parse(program_source)
-    if result == True:    
-        I = Interpreter()
-        I.run(parsed_program)
-        # print_line_nums(parsed_program[0])
-        # print(parsed_program)
-    else:
-        print('Parsing failed. There must have been a mismatched parenthesis.')
+#     file_path = "./codeExample3.brewin"
+#     program_source = read_file(file_path=file_path)
+#     # this is how you use our BParser class to parse a valid
+#     # Brewin program into python list format.
+#     result, parsed_program = BParser.parse(program_source)
+#     if result == True:    
+#         I = Interpreter()
+#         I.run(parsed_program)
+#         # print_line_nums(parsed_program[0])
+#         # print(parsed_program)
+#     else:
+#         print('Parsing failed. There must have been a mismatched parenthesis.')
 
-main()
+# main()
