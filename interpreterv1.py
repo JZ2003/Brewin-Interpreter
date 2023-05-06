18ac50bcbe246e29b913042aac86416c825a5da3
jz_2003 <zhaojianxing2003@gmail.com>
2023-05-05 22:28:53 -0700

    For gradescope test

--- interpreterv1.py
+++ interpreterv1.py
@@ -10,7 +10,6 @@ class Interpreter(InterpreterBase):
         self.BclassList = []
     
     def run(self,program):
-        Bobjects = []
         # class1 = Bclass(program,self)
         # classObject1 = class1.instantiate_object()
         # for f in classObject1.fields:
@@ -25,7 +24,8 @@ class Interpreter(InterpreterBase):
         # string = "\"\""
         # c = Bconstant(self,string)
         # print(f"string represents {c.value}, with type {c.type}")
-        for c in program:
+        result, parsed_program = BParser.parse(program)
+        for c in parsed_program:
             self.BclassList.append(Bclass(c,self))
         mainClass = next((c for c in self.BclassList if c.get_name() == INTBASE.MAIN_CLASS_DEF), (None,None))
         if mainClass == (None,None):
@@ -86,40 +86,40 @@ class Interpreter(InterpreterBase):
 
 
 
-def read_file(file_path):
-    """Reads a file of text and returns a list of strings."""
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-        lines = [line.strip() for line in lines]
-        return lines
+# def read_file(file_path):
+#     """Reads a file of text and returns a list of strings."""
+#     with open(file_path, 'r') as file:
+#         lines = file.readlines()
+#         lines = [line.strip() for line in lines]
+#         return lines
 
-def print_line_nums(parsed_program):
-    for item in parsed_program:
-        if type(item) is not list:
-            print(f'{item} was found on line {item.line_num}')
-        else:
-            print_line_nums(item)
+# def print_line_nums(parsed_program):
+#     for item in parsed_program:
+#         if type(item) is not list:
+#             print(f'{item} was found on line {item.line_num}')
+#         else:
+#             print_line_nums(item)
 
 
-def main():
-    # program_source = ['(class main',
-    # ' (method main ()',
-    # ' (print "hello world!")',
-    # ' ) # end of method',
-    # ') # end of class']
-    # program_source = ""
+# def main():
+#     # program_source = ['(class main',
+#     # ' (method main ()',
+#     # ' (print "hello world!")',
+#     # ' ) # end of method',
+#     # ') # end of class']
+#     # program_source = ""
 
-    file_path = "./codeExample2.brewin"
-    program_source = read_file(file_path=file_path)
-    # this is how you use our BParser class to parse a valid
-    # Brewin program into python list format.
-    result, parsed_program = BParser.parse(program_source)
-    if result == True:    
-        I = Interpreter()
-        I.run(parsed_program)
-        # print_line_nums(parsed_program[0])
-        # print(parsed_program)
-    else:
-        print('Parsing failed. There must have been a mismatched parenthesis.')
+#     file_path = "./codeExample3.brewin"
+#     program_source = read_file(file_path=file_path)
+#     # this is how you use our BParser class to parse a valid
+#     # Brewin program into python list format.
+#     result, parsed_program = BParser.parse(program_source)
+#     if result == True:    
+#         I = Interpreter()
+#         I.run(parsed_program)
+#         # print_line_nums(parsed_program[0])
+#         # print(parsed_program)
+#     else:
+#         print('Parsing failed. There must have been a mismatched parenthesis.')
 
 # main()
