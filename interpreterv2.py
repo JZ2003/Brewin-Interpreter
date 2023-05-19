91c7d3e39a8e054312bf98798abe57754f4aeb0d
jz_2003 <zhaojianxing2003@gmail.com>
2023-05-19 00:13:31 -0700

    For test.

--- interpreterv2.py
+++ interpreterv2.py
@@ -65,42 +65,42 @@ class Interpreter(InterpreterBase):
 
 
 
-def read_file(file_path):
-    """Reads a file of text and returns a list of strings."""
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-        lines = [line.strip() for line in lines]
-        return lines
+# def read_file(file_path):
+#     """Reads a file of text and returns a list of strings."""
+#     with open(file_path, 'r') as file:
+#         lines = file.readlines()
+#         lines = [line.strip() for line in lines]
+#         return lines
 
-# def print_line_nums(parsed_program):
-#     for item in parsed_program:
-#         if type(item) is not list:
-#             print(f'{item} was found on line {item.line_num}')
-#         else:
-#             print_line_nums(item)
+# # def print_line_nums(parsed_program):
+# #     for item in parsed_program:
+# #         if type(item) is not list:
+# #             print(f'{item} was found on line {item.line_num}')
+# #         else:
+# #             print_line_nums(item)
 
 
-def main():
-    # program_source = ['(class main',
-    # ' (method main ()',
-    # ' (print "hello world!")',
-    # ' ) # end of method',
-    # ') # end of class']
-    # program_source = ""
+# def main():
+#     # program_source = ['(class main',
+#     # ' (method main ()',
+#     # ' (print "hello world!")',
+#     # ' ) # end of method',
+#     # ') # end of class']
+#     # program_source = ""
 
-    file_path = "/Users/jayzee_robin/Desktop/CS131/P2/Spring23-CS131-Project-Starter/codeExample2.b++"
-    program_source = read_file(file_path=file_path)
-    I = Interpreter()
-    I.run(program=program_source)
-    # this is how you use our BParser class to parse a valid
-    # Brewin program into python list format.
-    # result, parsed_program = BParser.parse(program_source)
-    # if result == True:    
-    #     I = Interpreter()
-    #     I.test(parsed_program)
-    #     # print_line_nums(parsed_program[0])
-    #     # print(parsed_program)
-    # else:
-    #     print('Parsing failed. There must have been a mismatched parenthesis.')
+#     file_path = "/Users/jayzee_robin/Desktop/CS131/P2/Spring23-CS131-Project-Starter/codeExample2.b++"
+#     program_source = read_file(file_path=file_path)
+#     I = Interpreter()
+#     I.run(program=program_source)
+#     # this is how you use our BParser class to parse a valid
+#     # Brewin program into python list format.
+#     # result, parsed_program = BParser.parse(program_source)
+#     # if result == True:    
+#     #     I = Interpreter()
+#     #     I.test(parsed_program)
+#     #     # print_line_nums(parsed_program[0])
+#     #     # print(parsed_program)
+#     # else:
+#     #     print('Parsing failed. There must have been a mismatched parenthesis.')
 
-main()
+# main()
