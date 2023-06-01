ca3a56823d30f607426f7e3a571550a58936c721
jz_2003 <zhaojianxing2003@gmail.com>
2023-06-01 01:20:44 -0700

    Lil Try

--- Bfield.py
+++ Bfield.py
@@ -19,7 +19,7 @@ class Bfield:
         self.__parse_initial_value(initialValue)
     
     def __parse_initial_value(self,initialValue):
-        if initialValue is None or initialValue == INTBASE.NULL_DEF:
+        if initialValue is None:
             if self.fieldType == INTBASE.INT_DEF:
                 self.value = Bconstant(self.BASE,"0")
             elif self.fieldType == INTBASE.BOOL_DEF:
@@ -40,22 +40,21 @@ class Bfield:
                         self.value = Bnull(className=self.fieldType)
                     else:
                         self.BASE.error(ErrorType.TYPE_ERROR,description="There is no such template type!")
-
-
-        # elif initialValue == INTBASE.NULL_DEF: # Deal with object scenario
-        #     if not INTBASE.TYPE_CONCAT_CHAR in self.fieldType:
-        #         theClass = next((c for c in self.BASE.get_BclassList() if c.get_single_name() == self.fieldType), None)
-        #         if theClass is not None:
-        #             self.value = Bnull(className=theClass.get_name())
-        #         else:
-        #             self.BASE.error(ErrorType.TYPE_ERROR,description="There is no such class type!")
-        #     else:
-        #         plainTempType = self.fieldType.split(INTBASE.TYPE_CONCAT_CHAR)[0]
-        #         theTemplate = next((t for t in self.BASE.get_BtempList() if t.get_single_name() == plainTempType), None)
-        #         if theTemplate is not None:
-        #             self.value = Bnull(className=self.fieldType)
-        #         else:
-        #             self.BASE.error(ErrorType.TYPE_ERROR,description="There is no such template type!")
+        
+        elif initialValue == INTBASE.NULL_DEF: # Deal with object scenario
+            if not INTBASE.TYPE_CONCAT_CHAR in self.fieldType:
+                theClass = next((c for c in self.BASE.get_BclassList() if c.get_single_name() == self.fieldType), None)
+                if theClass is not None:
+                    self.value = Bnull(className=theClass.get_name())
+                else:
+                    self.BASE.error(ErrorType.TYPE_ERROR,description="There is no such class type!")
+            else:
+                plainTempType = self.fieldType.split(INTBASE.TYPE_CONCAT_CHAR)[0]
+                theTemplate = next((t for t in self.BASE.get_BtempList() if t.get_single_name() == plainTempType), None)
+                if theTemplate is not None:
+                    self.value = Bnull(className=self.fieldType)
+                else:
+                    self.BASE.error(ErrorType.TYPE_ERROR,description="There is no such template type!")
 
         else:  # Primitive scenario
             self.value = Bconstant(self.BASE,initialValue) 
